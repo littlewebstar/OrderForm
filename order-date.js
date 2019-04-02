fcad756d00f97f716d8ec99bcb5d90ce0439d0c1
Murphyst <murphyst@Ms-MacBook-Pro.local>
2019-04-02 16:21:12 +0100

    homework

--- order-date.js
+++ order-date.js
@@ -1,12 +1,28 @@
 function init(){
+  var currentDate = new Date().toLocaleDateString();
+  document.getElementById('orderDate').value = currentDate;
+
   var orderButton=document.getElementById('submitOrder')
   orderButton.addEventListener('click', (event) => orderSubmit(event));
 
+
+  var itemNameBox=document.getElementById('itemN');
   var amountBox=document.getElementById('amount');
   var unitPriceBox=document.getElementById('unitPrice');
-  amountBox.addEventListener('blur', (event) => calculateTotalAmount(amountBox, unitPriceBox));
+  var totalPriceBox=document.getElementById('totalPrice');
+  var deliveryOptions = document.getElementsByName('delivery');
+  var totalAllBox = document.getElementById('totalAll');
+  var orderDateBox = document.getElementById('orderDate');
+
+  orderDateBox.addEventListener('blur', (event) => orderSubmit(event));
+
+  itemNameBox.addEventListener('change', (event) => itemNumber(itemNameBox, unitPriceBox, totalPriceBox, amountBox, totalAllBox, deliveryOptions));
   
-  unitPriceBox.addEventListener('blur', (event)=> calculateTotalAmount(amountBox, unitPriceBox));
+  amountBox.addEventListener('change', (event)=> calculateTotalAmount(amountBox, unitPriceBox, totalPriceBox, totalAllBox, deliveryOptions));
+
+  deliveryOptions[0].addEventListener('click', (event) => addDeliveryFeeToTotal(totalAllBox, totalPriceBox, deliveryOptions));
+  deliveryOptions[1].addEventListener('click', (event) => addDeliveryFeeToTotal(totalAllBox, totalPriceBox , deliveryOptions));
+  deliveryOptions[2].addEventListener('click', (event) => addDeliveryFeeToTotal(totalAllBox, totalPriceBox, deliveryOptions));
 }
 
 function orderSubmit(event){
@@ -14,81 +30,50 @@ function orderSubmit(event){
     var dateValue=document.getElementById('orderDate').value;
     if(!validateDate(dateValue)){
       orderDateValidation.style.display='block';  
-    }   
+    } else{
+      orderDateValidation.style.display='none';  
+    }
 }
 
-function calculateTotalAmount(amountBox, unitPriceBox){
+function calculateTotalAmount(amountBox, unitPriceBox, totalPriceBox, totalAllBox, deliveryOptions){
   if(!amountBox.value ||  !unitPriceBox.value)
    return;
-   var totalPriceBox=document.getElementById('totalPrice');
-   totalPriceBox.value=amountBox.value * unitPriceBox.value;
+   totalPriceBox.value = amountBox.value * unitPriceBox.value;
+   addDeliveryFeeToTotal(totalAllBox,totalPriceBox, deliveryOptions);
+}
+
+function itemNumber(itemNameBox, unitPriceBox, totalPriceBox, amountBox, totalAllBox, deliveryOptions){
+  var i = itemNameBox.selectedIndex;
+    unitPriceBox.value = itemNameBox.options[i].value;
+    totalPriceBox.value = amountBox.value * unitPriceBox.value;
+    addDeliveryFeeToTotal(totalAllBox,totalPriceBox, deliveryOptions);
 }
 
 function validateDate(date){
-  /*validation code :
-    - Date will be validated on date input blur 
-    check if the date is valid date, 
-    for example: 32/03/2019 is invalid date
-                 30/02/2019 is invalid date
-    if the date is before the current date, it is invalid date 
-  */
-  return false;
-}
+  var dates = date.split('/');
+  var day = dates[0];
+  var month = dates[1];
+  var year = dates[2];
 
-function addDeliveryFeeToTotal(deliveryType){
-  /*
-    standard ==> 0 add,
-    fast ==> 2.99 add,
-    express => 4.99 add
-  */
+  var myDate = new Date(year, month-1, day);
+  var today = new Date();
+
+  return myDate < today
+    ?false
+    :true;
+}
+function addDeliveryFeeToTotal(totalAllBox, totalPriceBox, deliveryOptions){
+  var price = 0;
+  if(deliveryOptions[0].checked){
+    price = 0;
+  }else if(deliveryOptions[1].checked){
+    price = 2.99;
+  }else if(deliveryOptions[2].checked){
+    price = 4.99;
+  }
+  totalAllBox.value = `£${(Number(totalPriceBox.value) + price).toFixed(2)}`
 }
 
 init();
-
-
-/*
-  homework:
-  1- Implement validateDate
-  2- Implement addDeliveryFeeToTotal
-  3- Add items to array with each price, when item is selected
-  populate, unit price automatically.
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
